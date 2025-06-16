ba7a178428cef8b5133fdf91a55bf8a74cb38e9a
Raywall Malheiros <raywall.malheiros@gmail.com>
2025-06-16 12:45:32 -0300

    rocks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/gopher-rocks-sm.jpg',
+    image: 'img/gopher-rocks-sm.svg',  
     navbar: {
       title: 'Let`s Go',
       logo: {
